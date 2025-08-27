023c85695a2fe3b006d021d83fe5c540f0c1fc7c
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-08-26 22:47:46 -0700

    prompt user after they click addon from tool menu bar

--- __init__.py
+++ __init__.py
@@ -46,39 +46,51 @@ def maybe_prompt_install() -> None:
 
 def count_cards() -> None:
 
-    import spacy
+    box = QMessageBox(mw)
+    box.setIcon(QMessageBox.Icon.Information)
+    box.setWindowTitle("Anki Vocabulary Calculator")
+    box.setText("Which language would you like to calculate?")
+    # box.setDetailedText("Some detailed text")
+    japanese_button = box.addButton("Japanese 🇯🇵", QMessageBox.ButtonRole.AcceptRole)
+    cancel_button = box.addButton("Cancel", QMessageBox.ButtonRole.RejectRole)
+    box.setDefaultButton(japanese_button)
+    box.exec()
 
-    ja_words_txt_path = files(__package__) / "ja_words.txt"
-    with open(ja_words_txt_path, "r", encoding="utf-8") as file:
-        JA_WORD_LIST = file.read().splitlines()
+    if box.clickedButton() == japanese_button:
 
-    JA_NLP_MODEL = "ja_core_news_sm"
-    if not spacy.util.is_package(JA_NLP_MODEL):
-        spacy.cli.download(JA_NLP_MODEL)
-    nlp = spacy.load(JA_NLP_MODEL)
+        import spacy
 
-    all_card_ids = mw.col.find_cards("")
+        ja_words_txt_path = files(__package__) / "ja_words.txt"
+        with open(ja_words_txt_path, "r", encoding="utf-8") as file:
+            JA_WORD_LIST = file.read().splitlines()
 
-    lemma_retrievabilities = {}
-    for card_id in all_card_ids:
+        JA_NLP_MODEL = "ja_core_news_sm"
+        if not spacy.util.is_package(JA_NLP_MODEL):
+            spacy.cli.download(JA_NLP_MODEL)
+        nlp = spacy.load(JA_NLP_MODEL)
 
-        card_retrievability = mw.col.card_stats_data(card_id).fsrs_retrievability
+        all_card_ids = mw.col.find_cards("")
 
-        card_text = strip_html(mw.col.get_card(card_id).question())
-        doc = nlp(card_text)
+        lemma_retrievabilities = {}
+        for card_id in all_card_ids:
 
-        for token in doc:
+            card_retrievability = mw.col.card_stats_data(card_id).fsrs_retrievability
 
-            lemma = str(token.lemma_)
+            card_text = strip_html(mw.col.get_card(card_id).question())
+            doc = nlp(card_text)
 
-            if lemma in JA_WORD_LIST:
+            for token in doc:
 
-                if lemma not in lemma_retrievabilities or lemma_retrievabilities[lemma] < card_retrievability:
-                    lemma_retrievabilities[lemma] = card_retrievability
+                lemma = str(token.lemma_)
 
-    num_lemmas = int(sum(lemma_retrievabilities.values()))
+                if lemma in JA_WORD_LIST:
 
-    showInfo(f"You currently know at least {num_lemmas} Japanese base words.")
+                    if lemma not in lemma_retrievabilities or lemma_retrievabilities[lemma] < card_retrievability:
+                        lemma_retrievabilities[lemma] = card_retrievability
+
+        num_lemmas = int(sum(lemma_retrievabilities.values()))
+
+        showInfo(f"You currently know at least {num_lemmas} Japanese base words.")
 
 action = QAction("Anki Vocabulary Calculator")
 qconnect(action.triggered, count_cards)
